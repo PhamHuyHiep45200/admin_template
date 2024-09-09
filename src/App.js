62ea35b5ba14219b0d68bf1c7a5da503426a2aaa
Huy Hiep <hiepph3@rikkeisoft.com>
2024-09-09 10:09:28 +0700

    fix deploy

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Header, Footer, Sidebar } from "./components/admin";
-
-// import OwlCarousel from "./pages/Modules/OwlCarousel";
-// import Sparkline from "./pages/Modules/Sparkline";
-// import SweetAlert from "./pages/Modules/SweetAlert";
-// import Toastr from "./pages/Modules/Toastr";
-// import VectorMap from "./pages/Modules/VectorMap";
-// import WeatherIcon from "./pages/Modules/WeatherIcon";
 import { useLocation } from "react-router-dom";
-// import Subscribe from "./pages/Pages/Utilities/Subscribe";
-// import Contact from "./pages/Pages/Utilities/Contact";
-// import Invoice from "./pages/Pages/Utilities/Invoice";
-// import TransparentSidebar from "./pages/Strater/TransparentSidebar";
-// import Profile from "./pages/Pages/Features/Profile";
-// import SettingDetail from "./pages/Pages/Features/SettingDetail";
-// import Settings from "./pages/Pages/Features/Settings";
-// import Tickets from "./pages/Pages/Features/Tickets";
-// import Calender from "./pages/Modules/Calender";
-// import Chart from "./pages/Modules/Chart";
-// import DataTables from "./pages/Modules/DataTables";
-// import Flag from "./pages/Modules/Flag";
-// import FontAwesome from "./pages/Modules/FontAwesome";
-// import IonIcons from "./pages/Modules/IonIcons";
-// import Error403 from "./pages/Pages/Errors/403";
-// import Error404 from "./pages/Pages/Errors/404";
-// import Error500 from "./pages/Pages/Errors/500";
-// import Error503 from "./pages/Pages/Errors/503";
-// import Activities from "./pages/Pages/Features/Activities";
-// import PostCreate from "./pages/Pages/Features/PostCreate";
-// import Posts from "./pages/Pages/Features/Posts";
-
-
 import EcommerceDashboard from "./pages/Dashboard/EcommerceDashboard";
 import GeneralDashboard from "./pages/Dashboard/GeneralDashboard";
 import DefaultLayoutPage from "./pages/Strater/DefaultLayoutPage";
@@ -81,56 +51,52 @@ import FormEditor from "./pages/Forms/FormEditor";
 import FormAdvancedform from "./pages/Forms/FormAdvancedform";
 import ForgotPassword from "./pages/Pages/Auth/ForgotPassword";
 import Login from "./pages/Pages/Auth/Login";
-import Register from "./pages/Pages/Auth/Register";
 import ResetPassword from "./pages/Pages/Auth/ResetPassword";
 import Credit from "./pages/Pages/Credits";
 
+const Error403 = React.lazy(() => import("./pages/Pages/Errors/403"));
+const Error404 = React.lazy(() => import("./pages/Pages/Errors/404"));
+const Error500 = React.lazy(() => import("./pages/Pages/Errors/500"));
+const Error503 = React.lazy(() => import("./pages/Pages/Errors/503"));
 
-// const Posts = React.lazy(() => import('./pages/Pages/Features/Posts'));
-// const Posts = React.lazy(() => import('./pages/Pages/Features/Posts'));
-// const Posts = React.lazy(() => import('./pages/Pages/Features/Posts'));
-// const Posts = React.lazy(() => import('./pages/Pages/Features/Posts'));
-// const Posts = React.lazy(() => import('./pages/Pages/Features/Posts'));
-// const Posts = React.lazy(() => import('./pages/Pages/Features/Posts'));
+const Activities = React.lazy(() =>
+  import("./pages/Pages/Features/Activities")
+);
+const PostCreate = React.lazy(() =>
+  import("./pages/Pages/Features/PostCreate")
+);
+const Posts = React.lazy(() => import("./pages/Pages/Features/Posts"));
+const Profile = React.lazy(() => import("./pages/Pages/Features/Profile"));
 
-const Error403 = React.lazy(() => import('./pages/Pages/Errors/403'));
-const Error404 = React.lazy(() => import('./pages/Pages/Errors/404'));
-const Error500 = React.lazy(() => import('./pages/Pages/Errors/500'));
-const Error503 = React.lazy(() => import('./pages/Pages/Errors/503'));
+const SettingDetail = React.lazy(() =>
+  import("./pages/Pages/Features/SettingDetail")
+);
+const Settings = React.lazy(() => import("./pages/Pages/Features/Settings"));
+const Tickets = React.lazy(() => import("./pages/Pages/Features/Tickets"));
+const Calender = React.lazy(() => import("./pages/Modules/Calender"));
+const Chart = React.lazy(() => import("./pages/Modules/Chart"));
 
-const Activities = React.lazy(() => import('./pages/Pages/Features/Activities'));
-const PostCreate = React.lazy(() => import('./pages/Pages/Features/PostCreate'));
-const Posts = React.lazy(() => import('./pages/Pages/Features/Posts'));
-const Profile = React.lazy(() => import('./pages/Pages/Features/Profile'));
+const DataTables = React.lazy(() => import("./pages/Modules/DataTables"));
+const Flag = React.lazy(() => import("./pages/Modules/Flag"));
+const FontAwesome = React.lazy(() => import("./pages/Modules/FontAwesome"));
+const IonIcons = React.lazy(() => import("./pages/Modules/IonIcons"));
+const OwlCarousel = React.lazy(() => import("./pages/Modules/OwlCarousel"));
 
-const SettingDetail = React.lazy(() => import('./pages/Pages/Features/SettingDetail'));
-const Settings = React.lazy(() => import('./pages/Pages/Features/Settings'));
-const Tickets = React.lazy(() => import('./pages/Pages/Features/Tickets'));
-const Calender = React.lazy(() => import('./pages/Modules/Calender'));
-const Chart = React.lazy(() => import('./pages/Modules/Chart'));
+const Sparkline = React.lazy(() => import("./pages/Modules/Sparkline"));
+const SweetAlert = React.lazy(() => import("./pages/Modules/SweetAlert"));
+const Toastr = React.lazy(() => import("./pages/Modules/Toastr"));
+const VectorMap = React.lazy(() => import("./pages/Modules/VectorMap"));
 
-const DataTables = React.lazy(() => import('./pages/Modules/DataTables'));
-const Flag = React.lazy(() => import('./pages/Modules/Flag'));
-const FontAwesome = React.lazy(() => import('./pages/Modules/FontAwesome'));
-const IonIcons = React.lazy(() => import('./pages/Modules/IonIcons'));
-const OwlCarousel = React.lazy(() => import('./pages/Modules/OwlCarousel'));
+const WeatherIcon = React.lazy(() => import("./pages/Modules/WeatherIcon"));
+const Subscribe = React.lazy(() => import("./pages/Pages/Utilities/Subscribe"));
 
-const Sparkline = React.lazy(() => import('./pages/Modules/Sparkline'));
-const SweetAlert = React.lazy(() => import('./pages/Modules/SweetAlert'));
-const Toastr = React.lazy(() => import('./pages/Modules/Toastr'));
-const VectorMap = React.lazy(() => import('./pages/Modules/VectorMap'));
+const Invoice = React.lazy(() => import("./pages/Pages/Utilities/Invoice"));
+const TransparentSidebar = React.lazy(() =>
+  import("./pages/Strater/TransparentSidebar")
+);
+const Contact = React.lazy(() => import("./pages/Pages/Utilities/Contact"));
 
-const WeatherIcon = React.lazy(() => import('./pages/Modules/WeatherIcon'));
-const Subscribe = React.lazy(() => import('./pages/Pages/Utilities/Subscribe'));
-
-const Invoice = React.lazy(() => import('./pages/Pages/Utilities/Invoice'));
-const TransparentSidebar = React.lazy(() => import('./pages/Strater/TransparentSidebar'));
-const Contact = React.lazy(() => import('./pages/Pages/Utilities/Contact'));
-
-const history = React.lazy(() => import('./history'));
-
-
-
+const history = React.lazy(() => import("./history"));
 
 function App() {
   let location = useLocation().pathname;
@@ -139,42 +105,17 @@ function App() {
   let locationParent = locationSplit[1];
   let WithoutRouter = ["auth", "error", "utilities"];
 
-  // const RenderDataFullScreen = () => {
-  //   if (location === "/auth/forget-password") {
-  //     return <ForgotPassword />;
-  //   } else if (location === "/auth/register") {
-  //     return <Register />;
-  //   } else if (location === "/auth/reset-password") {
-  //     return <ResetPassword />;
-  //   } else if (location === "/error/503") {
-  //     return <Error503 />;
-  //   } else if (location === "/error/403") {
-  //     return <Error403 />;
-  //   } else if (location === "/error/404") {
-  //     return <Error404 />;
-  //   } else if (location === "/error/500") {
-  //     return <Error500 />;
-  //   } else if (location === "/utilities/subscribe") {
-  //     return <Subscribe />;
-  //   } else if (location === "/utilities/contact") {
-  //     return <Contact />;
-  //   }
-  // };
-
   return (
     <div className="App">
-      
-      <>
-        
-        {!WithoutRouter.includes(locationParent) ? (
-          <>
-            <Header />
-            <Sidebar />
-          </>
-        ) : (
-          ""
-        )}
-        <React.Suspense fallback={<h1>Still Loading…</h1>}>
+      {!WithoutRouter.includes(locationParent) ? (
+        <>
+          <Header />
+          <Sidebar />
+        </>
+      ) : (
+        ""
+      )}
+      <React.Suspense fallback={<h1>Still Loading…</h1>}>
         <Switch history={history}>
           <Route path="/" exact component={EcommerceDashboard} />
           <Route path="/dashboard/general" component={GeneralDashboard} />
@@ -270,9 +211,8 @@ function App() {
           <Route path="/utilities/subscribe" component={Subscribe} />
           <Route path="/utilities/contact" component={Contact} />
         </Switch>
-        </React.Suspense>
-        <Footer />
-      </>
+      </React.Suspense>
+      <Footer />
     </div>
   );
 }
